5e7b1b503d4df4f3edea689268365ae40a155f09
daniltirsk <56964428+daniltirsk@users.noreply.github.com>
2021-06-27 15:09:29 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,17 +6,22 @@ import html
 import time
 import os
 import datetime
+import sys
+
 from multiprocessing.pool import ThreadPool
 
 class DownloadError(Exception):
+    ''' raised when an error occurs while downloading'''
     pass
 
 class ParsingError(Exception):
+    ''' raised when an error occurs while parsing'''
     pass
 
 class InputError(Exception):
-    pass
+    ''' raised when user input is invalid'''
 
+#function for downloading xls files
 def download_xls(params):
     reg_type,filename = params
     table_url = 'http://register.ndda.kz/register.php/mainpage/reestr/lang/ru'
@@ -45,6 +50,7 @@ def download_xls(params):
     else:
         return [filename,data]
 
+#function for getting the names of the colums in a spreadsheet
 def get_col_names(sheet):
     col_names = []
     for rownum in range(sheet.nrows):
@@ -54,23 +60,27 @@ def get_col_names(sheet):
         break
     return col_names
 
+#function for processing and formatting string data
 def process_string_data(string):
     null_values = ['нет данных','()']
     result = string.strip()
     result = result.replace('\n','')
     result = html.unescape(result)
-    
+    #covert all null values to empty string
     if result.lower() in null_values:
         result = ''
     return result
 
+#function to check if an entry already exists
 def identity_check(parsed_data, reg_id):
     if reg_id in parsed_data:
         return False
     return True
-    
+
+#function to parse binary data as xls files   
 def parse_xls(files_to_parse):
-    #открываем файлы
+
+    #open files as workbooks
     docs = []
     try:
         for f in files_to_parse:
@@ -81,22 +91,22 @@ def parse_xls(files_to_parse):
     col_names = []
     parsed_data = {}
 
-    #обработка файлов
     for doc in docs:
-
+        #get the first spreadsheet in a workbook
         sheet = doc.sheet_by_index(0)
-        #заполняем массив имен
+        #get the names of the columns / keys for json
         if len(col_names) == 0:
             col_names = get_col_names(sheet)
 
-        #парсим таблицу
+        #parsing begins
         for rownum in range(1,sheet.nrows):
             row = sheet.row_values(rownum)
-            if identity_check(parsed_data, row[1]): #проверка на уникальность
+            if identity_check(parsed_data, row[1]): #check if unique
                 parsed_data[row[1]] = dict()
             
                 for i in range(2,len(row)):
-                    #если строка, приводим в нормальный вид
+                    #if string, then process and format
+                    #else append as is
                     if(isinstance(row[i],str)):
                         cell = process_string_data(row[i])
                     else:
@@ -105,7 +115,7 @@ def parse_xls(files_to_parse):
                 
     return parsed_data
 
-
+#function for saving dict in json format
 def json_save(path, parsed_data):
     jout = open(path, 'w', encoding='utf8')
     json.dump(parsed_data, jout, ensure_ascii=False, indent=4)
@@ -114,7 +124,8 @@ def json_save(path, parsed_data):
 if __name__ == '__main__':
 
     loop_time = time.time()
-    pause_time = 3 #пауза между выполнениями скрипта в секундах
+    pause_time = 3 #interval between launches
+    
     t_len = len(str(pause_time)) + 30
 
     while True:
@@ -129,9 +140,9 @@ if __name__ == '__main__':
             start_time = time.time()
     
             files_to_parse = []
-            
+
+            #start a session and download files asynchronously
             with requests.Session() as session:
-                #закачка файлов
                 types = [(1,'data_LSs.xls'), (2, 'data_MIs.xls')]
                 
                 downloaded_files = ThreadPool(len(types)).imap_unordered(download_xls, types)
@@ -140,13 +151,15 @@ if __name__ == '__main__':
                         if file == 'fail':
                             raise DownloadError
                             
-                        print('--- %s download is complete! it took %s seconds ---' % (filename, (time.time() - start_time)))
+                        print('--- %s download is complete! it took %s seconds  ---' % (filename, (time.time() - start_time)))
                         files_to_parse.append(file)
                 except DownloadError:
                     print("An error occured while downloading the files, script will restart later")
                     loop_time = time.time()
                     continue
-                    
+
+            #parse dowloaded files
+                
             start_time_parse = time.time()
             
             print('--- parsing begins ---')
@@ -161,6 +174,8 @@ if __name__ == '__main__':
             print('--- parsing complete in %s seconds ---' % (time.time() - start_time_parse))
             
             print('--- entries parsed = %i ---' % len(parsed_data))
+
+            #save parsed data in json format
             
             start_time_parse_save = time.time()
             
@@ -172,6 +187,7 @@ if __name__ == '__main__':
     
             print('--- total time %s seconds ---' % (time.time() - start_time))
         else:
+            #display time before launch
             if pause_time - (time.time() - loop_time) > 360:
                 print(('{0: <%i}' % t_len).format('%i hour(s) to start the script' % ((pause_time - (time.time() - loop_time))/360)), end='\r')
             elif pause_time - (time.time() - loop_time) > 60:
